fae5bf48f74952423065e867eb7d9935f40bfe31
deyviddfs <deyvid.dfs@gmail.com>
2022-06-19 18:54:52 -0300

    Tipos

---
+++ 05tipos/composicao/composicao.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+	//pode adicionar mais métodos
+}
+
+type bmw7 struct {
+}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Turbo...")
+}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("Baliza...")
+}
+
+func main() {
+	var c1 esportivoLuxuoso = bmw7{}
+	c1.fazerBaliza()
+	c1.ligarTurbo()
+}
